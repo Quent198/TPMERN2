11d299532658df261e30d2c0e4eeae51d706f49b
Quentin197 <145103225+Quent198@users.noreply.github.com>
2024-05-23 14:26:53 +0200

    Add files via upload

--- API/routes/users.js
+++ API/routes/users.js
@@ -1,10 +1,12 @@
 const {
   signupUser,
+  loginUser,
 } = require("../controllers/user-controller");
 
 const router = require("express").Router();
 
 router.post("/signup", signupUser);
+router.post("/signin", loginUser);
 
 
 
